12e02737beb723d3e0f5cefc5956db4fbf94ad16
Alberto Basalo <albertobasalo@hotmail.com>
2021-03-25 12:59:58 +0100

    chore: add environment api url

--- apps/www/src/environments/environment.prod.ts
+++ apps/www/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
   production: true,
+  apiUrl: 'https://api-angular-builders.herokuapp.com/v1',
 };
--- apps/www/src/environments/environment.ts
+++ apps/www/src/environments/environment.ts
@@ -4,6 +4,7 @@
 
 export const environment = {
   production: false,
+  apiUrl: 'https://api-angular-builders.herokuapp.com/v1',
 };
 
 /*
