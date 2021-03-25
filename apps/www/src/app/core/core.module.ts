8464543f6cfe073785eed7403334fa9bced1876b
Alberto Basalo <albertobasalo@hotmail.com>
2021-03-25 15:27:10 +0100

    refactor: move httpclientmodule to core

--- apps/www/src/app/core/core.module.ts
+++ apps/www/src/app/core/core.module.ts
@@ -1,11 +1,12 @@
 import { LayoutModule } from '@ab/layout';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { CoreRoutingModule } from './core-routing.module';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, CoreRoutingModule, LayoutModule],
+  imports: [CommonModule, CoreRoutingModule, HttpClientModule, LayoutModule],
   exports: [LayoutModule],
 })
 export class CoreModule {}
