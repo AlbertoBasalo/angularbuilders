295a966cd52d42988df2786452b7f61aa9cf028c
Alberto Basalo <albertobasalo@hotmail.com>
2021-03-25 13:21:39 +0100

    feat: show-category-list-at-home-page - Closes #5

--- apps/www/src/app/app.module.ts
+++ apps/www/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ServiceWorkerModule } from '@angular/service-worker';
@@ -10,6 +11,7 @@ import { CoreModule } from './core/core.module';
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     CoreModule,
+    HttpClientModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
     }),
--- libs/domain/home/src/lib/home.service.ts
+++ libs/domain/home/src/lib/home.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HomeService {
-
-  constructor() { }
+  private readonly categoriesUrl =
+    'https://api-angular-builders.herokuapp.com/v1' + '/categories';
+  constructor(private http: HttpClient) {}
+  getCategories$() {
+    return this.http
+      .get<any>(this.categoriesUrl)
+      .pipe(map((result) => result['data']));
+  }
 }
