744e9635cb09430946042a5a68d4ab7a8eaa7c05
Alberto Basalo <albertobasalo@hotmail.com>
2021-03-26 14:01:00 +0100

    feat: lead-form

--- libs/domain/home/src/lib/home.module.ts
+++ libs/domain/home/src/lib/home.module.ts
@@ -1,17 +1,20 @@
+import { FormModule } from '@ab/form';
 import { UiModule } from '@ab/ui';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CategoriesComponent } from './categories/categories.component';
 import { HomePage } from './home.page';
+import { LeadForm } from './lead/lead.form';
 @NgModule({
   imports: [
     CommonModule,
+    FormModule,
     RouterModule.forChild([
       { path: '', pathMatch: 'full', component: HomePage },
     ]),
     UiModule,
   ],
-  declarations: [HomePage, CategoriesComponent],
+  declarations: [HomePage, CategoriesComponent, LeadForm],
 })
 export class HomeModule {}
