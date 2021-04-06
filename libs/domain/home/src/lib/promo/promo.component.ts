871ecdf145f438e54a6184394c3663f6f1f999f9
Alberto Basalo <albertobasalo@hotmail.com>
2021-04-06 17:02:21 +0200

    refactor: rename promo component

--- libs/domain/home/src/lib/promo/promo.component.ts
+++ libs/domain/home/src/lib/promo/promo.component.ts
@@ -1,15 +1,15 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'ab-services',
-  templateUrl: './services.component.html',
+  selector: 'ab-promo',
+  templateUrl: './promo.component.html',
   styleUrls: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ServicesComponent implements OnInit {
+export class PromoComponent implements OnInit {
   header = {
     heroClass: 'is-info',
-    title: 'Services by Angular.Builders',
+    title: 'Promo by Angular.Builders',
     subtitle:
       'Advise, consulting and learning services by Angular seasoned professionals',
   };
