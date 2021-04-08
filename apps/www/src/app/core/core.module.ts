15beb1b2f52a2b7e80d350de18ca89c1371d43be
Alberto Basalo <albertobasalo@hotmail.com>
2021-04-08 11:56:30 +0200

    feat: Search-box-widget - Closes #22

--- apps/www/src/app/core/core.module.ts
+++ apps/www/src/app/core/core.module.ts
@@ -1,20 +1,28 @@
 import { ENVIRONMENT } from '@ab/global';
 import { LayoutModule } from '@ab/layout';
+import { SearchBoxModule } from '@ab/search-box';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { CoreRoutingModule } from './core-routing.module';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [LayoutComponent],
   providers: [
     {
       provide: ENVIRONMENT,
       useValue: environment,
     },
   ],
-  imports: [CommonModule, CoreRoutingModule, HttpClientModule, LayoutModule],
-  exports: [LayoutModule],
+  imports: [
+    CommonModule,
+    CoreRoutingModule,
+    HttpClientModule,
+    LayoutModule,
+    SearchBoxModule,
+  ],
+  exports: [LayoutModule, LayoutComponent],
 })
 export class CoreModule {}
--- libs/domain/search-box/src/lib/search-box/search-box.component.ts
+++ libs/domain/search-box/src/lib/search-box/search-box.component.ts
@@ -1,17 +1,49 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { fromEvent, Subscription } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
 
 @Component({
   selector: 'ab-search-box',
   templateUrl: './search-box.component.html',
-  styles: [
-  ],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styles: [],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SearchBoxComponent implements OnInit {
+export class SearchBoxComponent implements AfterViewInit, OnDestroy {
+  @Input() set initial(value: string) {
+    if (this.searchInput && !!value) {
+      this.searchInput.nativeElement.value = value;
+    }
+  }
+  @Output() search = new EventEmitter<string>();
+  @ViewChild('searchInput') searchInput!: ElementRef;
 
-  constructor() { }
+  private inputSubscription!: Subscription;
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.inputSubscription = fromEvent<any>(
+      this.searchInput.nativeElement,
+      'keyup'
+    )
+      .pipe(
+        map((event) => event.target.value),
+        map((value) => value as string),
+        debounceTime(400),
+        distinctUntilChanged()
+      )
+      .subscribe((searchTerm) => this.search.next(searchTerm));
   }
 
+  ngOnDestroy(): void {
+    this.inputSubscription.unsubscribe();
+  }
 }
