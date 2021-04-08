0c0048a1c38b6d7c1c0508e06fc98fc6e8b5724c
Alberto Basalo <albertobasalo@hotmail.com>
2021-04-08 13:11:11 +0200

    feat: Search-Page - Closes #24

--- libs/domain/search/src/lib/search.module.ts
+++ libs/domain/search/src/lib/search.module.ts
@@ -1,6 +1,8 @@
-import { NgModule } from '@angular/core';
+import { UiModule } from '@ab/ui';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ResultList } from './result-list/result.list';
 import { SearchPage } from './search.page';
 import { SearchComponent } from './search/search.component';
 
@@ -8,9 +10,10 @@ import { SearchComponent } from './search/search.component';
   imports: [
     CommonModule,
     RouterModule.forChild([
-      /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
+      { path: '', pathMatch: 'full', component: SearchPage },
     ]),
+    UiModule,
   ],
-  declarations: [SearchPage, SearchComponent],
+  declarations: [SearchPage, SearchComponent, ResultList],
 })
 export class SearchModule {}
