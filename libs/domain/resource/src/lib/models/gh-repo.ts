293f433635b5d635e4023974e8c72b0f76c008eb
Alberto Basalo <albertobasalo@hotmail.com>
2021-06-30 20:01:52 +0200

    feat: github details fro resources

--- libs/domain/resource/src/lib/models/gh-repo.ts
+++ libs/domain/resource/src/lib/models/gh-repo.ts
@@ -1,8 +1,11 @@
 export interface GhRepo {
+  name: string;
+  full_name: string;
   html_url: string;
   description: string;
   updated_at: Date;
   stargazers_count: number;
   subscribers_count: number;
   homepage: string;
+  language: string;
 }
